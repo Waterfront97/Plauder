7620d38d171b6dfb71be69d1117c4c248e9c239f
Joba <admin@serverlein.de>
2018-05-27 20:18:39 +0200

    Allow all image formats

--- Client/src/app/pipes/imgify.pipe.ts
+++ Client/src/app/pipes/imgify.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class ImgifyPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    const imgRegex = new RegExp('data:image\\/png;base64,.*');
+    const imgRegex = new RegExp('data:image\\/.*;base64,.*');
     const d = value.match(imgRegex);
     if (d === undefined || d === null || d.length === 0 || d.length > 1) {
       return value;
